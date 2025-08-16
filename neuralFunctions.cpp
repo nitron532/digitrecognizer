3f5f1b5a4d880153a2c62b1a77a9d9a343ccc85c
nitron532 <89716148+nitron532@users.noreply.github.com>
2025-08-15 21:09:51 -0700

    updates to avoid mixing matrix multiplication with elementwise operations, sgd progress

--- neuralFunctions.cpp
+++ neuralFunctions.cpp
@@ -1,5 +1,11 @@
 #include "neuralFunctions.h"
 //consider passing things by reference for efficiency ( sigmoid functions)
+auto sigmoid(Eigen::MatrixXd z){ //might wanna change the auto to the actual type?
+    return 1.0/(1.0 + (-z).array().exp());
+} 
+auto sigmoidPrime(Eigen::MatrixXd z){
+    return sigmoid(z)*(1-sigmoid(z));
+}
 Network::Network(std::vector<int>& lS){
     numLayers = layerSizes.size();
     for(size_t i = 1; i < numLayers; i++){
@@ -13,16 +19,35 @@ Network::Network(std::vector<int>& lS){
 
 auto Network::feedForward(Eigen::VectorXd& prevLayerOutputs){
     for(size_t i = 0; i < biases.size(); i++){
-        prevLayerOutputs = (weights[i] * prevLayerOutputs + biases[i]).unaryExpr(&sigmoid);
+        Eigen::VectorXd z = weights[i] * prevLayerOutputs + biases[i];
+        prevLayerOutputs = sigmoid(z);
     }
     return prevLayerOutputs;
 }
 
-
-
-auto sigmoid(Eigen::MatrixXd z){ //might wanna change the auto to the actual type?
-    return 1.0/(1.0 + (-z).exp());
-} 
-auto sigmoidPrime(Eigen::MatrixXd z){
-    return sigmoid(z)*(1-sigmoid(z));
+void Network::updateMiniBatch(std::vector<std::pair<double,int>> batch, double lR){
+    
 }
+
+void Network::stochasticGradientDescent(std::vector<std::pair<double,int>> trainingData, size_t epochs, size_t miniBatchSize,double learningRate, std::vector<std::pair<double,int>> testData){
+    size_t trainingDataSize = trainingData.size();
+    std::random_device rd;
+    std::mt19937 g(rd());
+    for(size_t i = 0; i < epochs; i++){
+        std::shuffle(trainingData.begin(), trainingData.end(),g);
+        std::vector<std::vector<std::pair<double,int>>> miniBatches;
+        for(size_t k = 0; k < trainingData.size(); k+=miniBatchSize){
+            std::vector<std::pair<double,int>> batch(trainingData.begin()+k, trainingData.begin()+k+miniBatchSize);
+            miniBatches.push_back(batch);
+        }
+        for(auto miniBatch : miniBatches){
+            updateMiniBatch(miniBatch,learningRate);
+        }
+        // if(testData.size() != 0){
+        //     std::cout<< "Epoch: " << i+1 << evaluate(test)
+        // }
+    }
+}
+
+
+
--- neuralFunctions.h
+++ neuralFunctions.h
@@ -2,6 +2,10 @@
 #include <vector>
 #include <cmath>
 #include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
+#include <algorithm>
+#include <random>
+#include <unordered_map>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 class Network{
@@ -10,7 +14,9 @@ class Network{
         std::vector<int> layerSizes;
         std::vector<Eigen::VectorXd> biases;
         std::vector<Eigen::MatrixXd> weights;
+        void updateMiniBatch(std::vector<std::pair<double,int>> batch, double lR);
     public:
         Network(std::vector<int>& lS);
         auto feedForward(Eigen::VectorXd& previousLayerOutput);
+        void stochasticGradientDescent(std::vector<std::pair<double,int>> trainingData, size_t epochs, size_t miniBatchSize, double learningRate,std::vector<std::pair<double,int>> testData);
 };
